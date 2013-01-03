a967cbaca9e48659add992cd96425f2e2c5715dd
Juliana Lucena <julianalucenaa@gmail.com>
2013-01-03 15:47:44 -0300

    Sets ActiveRecord as backend.

--- lib/untied-consumer-sync-activerecord.rb
+++ lib/untied-consumer-sync-activerecord.rb
@@ -1,12 +1,3 @@
-require "untied-consumer-sync-activerecord/version"
-require "untied-consumer-sync-activerecord/backend/active_record"
 require "untied-consumer-sync"
 
-module Untied
-  module Consumer
-    module Sync
-      module Activerecord
-      end
-    end
-  end
-end
+Untied::Consumer::Sync.backend = :active_record
